721ae4c8c96f276656aa59b8fd33e5f768ad35db
miguelcpmarques <miguelcdpmarques@gmail.com>
2018-09-10 11:58:20 +0100

    Updated widgets for responsiveness

--- res_operacionais.py
+++ res_operacionais.py
@@ -1,4 +1,3 @@
-
 from datetime import datetime, date
 
 from bokeh.io import curdoc
@@ -10,8 +9,9 @@ from bokeh.plotting import figure
 
 from generate_data import generate_df
 
-dataset = generate_df()
+dataset = generate_df() # original
 dataset['Data'] = [i.to_pydatetime().date() for i in dataset['Data']]
+current_dataset = dataset.copy() # filtered by widgets
 
 #==================================================
 # Main plot
@@ -25,6 +25,9 @@ meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun',
 options = ["Vendas", "Margem_Operacional", "EBITDA"]
 colors = ['#c6dbef', '#6baed6', '#084594']
 colormapper = dict(zip(options,colors))
+mapa_produtos = {0:"Produto_A", 1:"Produto_B", 2:"Produto_C"}
+
+
 
 source = ColumnDataSource(data=dict(x=[], y=[], labels=[], color=[]))
 # initialize widgets
@@ -32,25 +35,38 @@ select = Select(value="Vendas", options=options)
 dates = DateRangeSlider(start=date(2017, 1, 1),
                         end=date(2018, 9, 1),
                         value=(date(2017, 1, 1), date(2018, 9, 1)), step=1)
-produtos = CheckboxGroup(labels=["Produto A", "Produto B", "Produto C"],
+produtos = CheckboxGroup(labels=["Produto_A", "Produto_B", "Produto_C"],
                          active=[0, 1, 2])
 div1 = Div(text="<br><h5>Medida</h5>")
 div2 = Div(text="<br><h5>Datas</h5>")
 div3 = Div(text="<br><h5>Produtos</h5>")
 # hover = HoverTool(tooltips=[("word", "@words")])
 
-def refresh_data(dataset):
-    dataset = dataset.groupby('Ano/Mês')[select.value].sum().reset_index()
-    x = dataset['Ano/Mês']
-    y = dataset[select.value]
+def refresh_data(dataset, init=0):
+    """Refreshes the source.data based on the dataset provided"""
+    if init == 1:
+        start_date, end_date = dates.value
+        dataset_filtered = dataset[(dataset['Data']>=start_date) & (dataset['Data']<=end_date)]
+    else:
+        start_date = datetime.fromtimestamp(dates.value[0]/1000).date()
+        end_date = datetime.fromtimestamp(dates.value[1]/1000).date()
+        dataset_filtered = dataset[(dataset['Data']>=start_date) & (dataset['Data']<=end_date)]
+    # Apply filters
+    dataset_filtered = dataset[(dataset['Data']>=start_date) & (dataset['Data']<=end_date)]
+    lista_produtos = [mapa_produtos[i] for i in produtos.active]
+    dataset_filtered = dataset_filtered[dataset_filtered['Produto'].isin(lista_produtos)]
+    # Apply calculations
+    dataset_filtered = dataset_filtered.groupby('Ano/Mês')[select.value].sum().reset_index()
+    x = dataset_filtered['Ano/Mês']
+    y = dataset_filtered[select.value]
     labels = ["{}m €".format(int(i)) for i in y]
     color = [colormapper[select.value] for i in y]
     source.data = {'x': x,
                    'y': y,
                    'labels': labels,
                    'color': color}
-    print("Is the Error here?\n")
-refresh_data(dataset) # initialize data
+
+refresh_data(dataset, init=1) # initialize data
 
 
 # Creating the figure
@@ -64,41 +80,49 @@ plot = figure(x_range=FactorRange(*x_range), y_range=(0, max(source.data['y'])*1
               sizing_mode='stretch_both',
               toolbar_location=None)
 
-# Adding the vertical bars
 plot.vbar(x='x', top='y', width=0.7, source=source, fill_color='color')
 
 # Adding custom labels
-labels = LabelSet(x='x', y='y', text='labels', source=source, level='glyph',render_mode='css',
-                 text_font='times',text_font_size='11px',text_font_style='bold',
-                 y_offset=5,text_align='center',text_color='black')
-plot.add_layout(labels)
+# labels = LabelSet(x='x', y='y', text='labels', source=source, level='glyph',render_mode='css',
+#                  text_font='times',text_font_size='11px',text_font_style='bold',
+#                  y_offset=5,text_align='center',text_color='black')
+# plot.add_layout(labels)
 
 
-# Function to generate new data on button click
 def generate_data():
+    """Generates new data on button click"""
     dataset = generate_df()
     refresh_data(dataset=dataset)
 
 button = Button(label="Gerar novos dados", button_type="primary")
 button.on_click(generate_data)
 
-# Function to update dropdown on change
 def update_medida(attrname, old, new):
+    """Updates medida being used on select change"""
     plot.title.text = select.value
-    source.data = refresh_data(dataset)
+    refresh_data(dataset)
 
 select.on_change('value', update_medida)
 
-# Function to update dropdown on change
-def update_data(attrname, old, new):
+def update_date(attrname, old, new):
+    """Updates data on DateRangeSlider change"""
     start_date = datetime.fromtimestamp(dates.value[0]/1000).date()
-    end_date = datetime.fromtimestamp(dates.value[0]/1000).date()
-    global dataset
-    dataset = dataset[(dataset['Data']>=start_date)&(dataset['Data']<=end_date)]
+    end_date = datetime.fromtimestamp(dates.value[1]/1000).date()
+    dataset_filtered = dataset.copy()
+    dataset_filtered = dataset_filtered[(dataset_filtered['Data']>=start_date) & (dataset_filtered['Data']<=end_date)]
+    refresh_data(dataset_filtered)
 
-    source.data = refresh_data(dataset)
+dates.on_change('value', update_date)
 
-dates.on_change('value', update_data)
+def update_produtos(attrname, old, new):
+    """Updates Produtos on CheckboxGroup change"""
+    lista_produtos = [mapa_produtos[i] for i in produtos.active]
+    dataset_filtered = dataset.copy()
+    dataset_filtered = dataset_filtered[dataset_filtered['Produto'].isin(lista_produtos)]
+    refresh_data(dataset_filtered)
+
+produtos.on_change('active',update_produtos)
+
 
 # Define static layouts
 plot.sizing_mode = 'scale_width'
